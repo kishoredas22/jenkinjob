524fd2404f5d4b83766804df586e954a41aefa3c
kishoredas22 <77960982+kishoredas22@users.noreply.github.com>
2021-06-11 21:12:09 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,7 +1,5 @@
-class Hello
-{
-public static void main(String args[])
-{
-System.out.println("Hello, World..!");
-}
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+    }
 }
