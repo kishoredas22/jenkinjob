f7df250c69c92c87dcba59e3e8f26ca19b353945
kishoredas22 <77960982+kishoredas22@users.noreply.github.com>
2021-08-31 17:32:00 +0530

    Update and rename Hello.java to main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+
+
+main.py
+import mysql.connector
+mydb =mysql.connector.connect(host="localhost", user="root",passwd="password@123",database="kishoredb")
+
+mycursor =mydb.cursor()
+
+mycursor.execute("Create database kishoredb")
+
